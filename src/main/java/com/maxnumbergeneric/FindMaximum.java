166d91e43daa4852f5b253d98a1fe832b96bb8d6
namdev1994 <n.lavhare1994@gmail.com>
2021-05-18 16:40:12 +0530

    Added: Find Integr maximum number

--- src/main/java/com/maxnumbergeneric/FindMaximum.java
+++ src/main/java/com/maxnumbergeneric/FindMaximum.java
@@ -4,5 +4,17 @@ public class FindMaximum {
     public static void main(String[] args) {
         System.out.println("find Maximum Number ");
     }
-
+    public Integer findMaximum(Integer i,Integer j,Integer k) // method to check maximum Number
+    {
+        Integer max=i;
+        if(j.compareTo(max)>0)
+        {
+            max=j;
+        }
+        if(k.compareTo(max)>0)
+        {
+            max=k;
+        }
+        return max;
+    }
 }
--- src/test/java/com/maxnumbergeneric/FindMaximumTest.java
+++ src/test/java/com/maxnumbergeneric/FindMaximumTest.java
@@ -4,6 +4,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FindMaximumTest {
-
+    @Test
+    public void firstNumberTest()// positive test for maximum number
+    {
+        FindMaximum findMaximum = new FindMaximum();
+        Integer maximum = findMaximum.findMaximum(12, 9, 4);//pass the method parammetr to check
+        Assertions.assertEquals(12,maximum);
+    }
+    @Test
+    public void firstNumberNegativeTest()//negative test
+    {
+        FindMaximum findMaximum = new FindMaximum();
+        Integer maximum = findMaximum.findMaximum(12, 9, 4);
+        Assertions.assertEquals(9,maximum);
+    }
 
 }
