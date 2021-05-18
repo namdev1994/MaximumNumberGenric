59edd4e49342f9238ec0370d4f5b8e4ddaa3104b
namdev1994 <n.lavhare1994@gmail.com>
2021-05-18 11:20:08 +0530

    Added:Welcome message in Find maximum number progrem

---
+++ src/main/java/com/maxnumbergeneric/FindMaximum.java
@@ -0,0 +1,8 @@
+package com.maxnumbergeneric;
+
+public class FindMaximum {
+    public static void main(String[] args) {
+        System.out.println("find Maximum Number ");
+    }
+
+}
---
+++ src/test/java/com/maxnumbergeneric/FindMaximumTest.java
@@ -0,0 +1,9 @@
+package com.maxnumbergeneric;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FindMaximumTest {
+
+
+}
